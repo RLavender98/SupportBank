a1a6c6ca65578bcfdfd3f29fd0537d5fa0724cdb
Ruby Lavender <ruby.lavender@softwire.com>
2020-07-08 11:10:19 +0100

    Added a lttle logging

--- SupportBank/JsonReader.cs
+++ SupportBank/JsonReader.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
 using Newtonsoft.Json;
+using NLog;
 
 namespace SupportBank
 {
     internal class JsonReader
     {
+        private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
         public List<Transaction> ReadJson(List<Transaction> transactions, string path)
         {
             string fileData =
@@ -12,6 +14,7 @@ namespace SupportBank
             fileData = fileData.Replace("FromAccount", "Payer").Replace("ToAccount", "Payee");
             var jsonFile = JsonConvert.DeserializeObject<List<Transaction>>(fileData);
             transactions.AddRange(jsonFile);
+            logger.Info("Added "+path+" to the list of transactions.");
             return transactions;
         }
     }
